2455ae19b130ca2f6f50845a6c5187bb63a3a60d
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-04-14 19:42:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    dts({
-      insertTypesEntry: true
-    })
   ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/lib/index.tsx'),
-      name: 'FabricJS React',
-      fileName: (format) => `fabricjs-react.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'fabric'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React'
-        }
-      }
-    }
-  }
 })
